4db2526d243887b0f7c7f34bbd00a212c7c95815
James Davis <jamesdavis1727@gmail.com>
2022-05-30 15:48:23 -0400

    MenuComponent

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+/* eslint-disable react/react-in-jsx-scope */
 import logo from './logo.svg';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Blandford Pool Service</p>
+        Powered by 
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://capjimllc.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+        CapJim Technologies LLC  
         </a>
       </header>
+      <Menu />
     </div>
   );
 }
